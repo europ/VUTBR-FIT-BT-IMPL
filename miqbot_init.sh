cff8e1655d0856956cbb9955acf7ea038d768d3e
Adrián Tóth <adricko28@gmail.com>
2018-05-05 17:19:01 +0200

    psql 2

--- miqbot_init.sh
+++ miqbot_init.sh
@@ -24,9 +24,14 @@ sed -i "s/password:.*/password: miqbot/g" config/database.yml
 echo -e "\033[1m\033[31mSTOPPED BEFORE PSQL SECTION\033[0m"
 exit 1
 
-#TODO
-
+postgresql-setup initdb
+grep -q '^local\s' /var/lib/pgsql/data/pg_hba.conf || echo "local all all trust" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
+sed -i.bak 's/\(^local\s*\w*\s*\w*\s*\)\(peer$\)/\1trust/' /var/lib/pgsql/data/pg_hba.conf
+systemctl enable postgresql
+systemctl start postgresql
 sudo -u postgres psql -c "CREATE ROLE miqbot SUPERUSER LOGIN PASSWORD 'miqbot'"
+sed -i "s/^local\s*all\s*all\s*ident$/local all all md5/g" /var/lib/pgsql/data/pg_hba.conf
+systemctl restart postgresql.service
 
 bundle exec rake db:setup
 
