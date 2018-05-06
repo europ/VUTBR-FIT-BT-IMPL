78aaa9676dce9d20b0e2eaac25de22642dab88cf
Adrián Tóth <adricko28@gmail.com>
2018-05-06 13:37:40 +0200

    pg_hba.conf

--- miqbot_init.sh
+++ miqbot_init.sh
@@ -29,15 +29,20 @@ redis-cli ping
 cp config/database.tmpl.yml config/database.yml
 sed -i "s/username:.*/username: miqbot/g" config/database.yml
 sed -i "s/password:.*/password: miqbot/g" config/database.yml
+
 postgresql-setup initdb
 grep -q '^local\s' /var/lib/pgsql/data/pg_hba.conf || echo "local all all trust" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
 sed -i.bak 's/\(^local\s*\w*\s*\w*\s*\)\(peer$\)/\1trust/' /var/lib/pgsql/data/pg_hba.conf
 systemctl enable postgresql
 systemctl start postgresql
 sudo -u postgres psql -c "CREATE ROLE miqbot SUPERUSER LOGIN PASSWORD 'miqbot'"
+
+echo -e "\033[1m\033[33mModifying /var/lib/pgsql/data/pg_hba.conf\033[0m"
 sed -i "s/^\(local\s*all\s*all\s*\)trust$/\1md5/g" /var/lib/pgsql/data/pg_hba.conf
 sed -i "s/^\(host\s*all\s*all\s*.*\s*\)ident$/\1md5/g" /var/lib/pgsql/data/pg_hba.conf
+
 systemctl restart postgresql.service
+
 bundle exec rake db:setup
 
 echo -e "\033[32mPostgreSQL done.\033[0m"
